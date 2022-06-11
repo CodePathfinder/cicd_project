4037e87b569c69a01ebe99d67324a2ec767cf8dd
Gennadiy Molchanov <gamolchanov@gmail.com>
2022-06-12 02:54:38 +0300

    refactor webserver name

--- output.tf
+++ output.tf
@@ -3,9 +3,17 @@
 #########################################
 
 output "PublicIP" {
-  value = aws_instance.webserver.public_ip
+  value = aws_instance.webserver-a.public_ip
 }
 
 output "PrivateIP" {
-  value = aws_instance.webserver.private_ip
+  value = aws_instance.webserver-a.private_ip
+}
+
+output "PublicIP" {
+  value = aws_instance.webserver-b.public_ip
+}
+
+output "PrivateIP" {
+  value = aws_instance.webserver-b.private_ip
 }
