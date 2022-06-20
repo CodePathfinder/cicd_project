610aa97e13f8d30eaf7c0c8c52d380c0b5ab3ea5
CodePathfinder <63614129+CodePathfinder@users.noreply.github.com>
2022-06-20 23:49:00 +0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -22,7 +22,8 @@ resource "aws_vpc_peering_connection" "default_main_peering" {
   vpc_id      = aws_vpc.main.id
   peer_vpc_id = "vpc-0dd796b7b6beec76f"
   auto_accept = true
-  
+
+  /*
   accepter {
     allow_remote_vpc_dns_resolution = true
   }
@@ -30,22 +31,22 @@ resource "aws_vpc_peering_connection" "default_main_peering" {
   requester {
     allow_remote_vpc_dns_resolution = true
   }
-}
-
+} */
+  
 resource "aws_route" "default_vpc_rt" {
   route_table_id            = "rtb-093ac07e6f8726c61"
   destination_cidr_block    = "10.0.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.default_main_peering.id
   depends_on                = [aws_vpc_peering_connection.default_main_peering]
 }
-
+/*
 resource "aws_route" "main_vpc_rt" {
   route_table_id            = aws_vpc.main.main_route_table_id
   destination_cidr_block    = "172.31.0.0/24"
   vpc_peering_connection_id = aws_vpc_peering_connection.default_main_peering.id
   depends_on                = [aws_vpc_peering_connection.default_main_peering]
 }
-
+*/
 #########################################
 # ======== CREATE PUBLIC SUBNET =========
 #########################################
