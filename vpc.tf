b019a17be5cbc08627dfec76f6e158fe7cbf4d69
Gennadiy Molchanov <gamolchanov@gmail.com>
2022-06-25 01:31:38 +0300

    asg in my vpc

--- vpc.tf
+++ vpc.tf
@@ -218,3 +218,42 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.private_subnets[*].id
 }
+
+
+
+
+/*
+###############################################
+# == Elastic Load Balancer (Internet-Faced) ==
+###############################################
+
+resource "aws_elb" "web" {
+  name = "web-elb"
+  subnets = [aws_subnet.public_subnets[*].id]
+  security_groups = [aws_security_group.web.id]
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = 80
+    instance_protocol = "http"
+  }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+  instances = aws_instance.webserver[*].id
+  cross_zone_load_balancing   = true
+
+  tags = {
+    Name = "${var.env}-web-elb"
+    Project = var.project
+  }
+}
+
+output "elb_address" {
+  value = aws_elb.web.dns_name
+}
+*/
