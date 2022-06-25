14df121b17a090f70713b5caf67431934e028bd0
Gennadiy Molchanov <gamolchanov@gmail.com>
2022-06-25 17:45:33 +0300

    pipelines

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-cicd"
-    key    = "terraform/backend"
-    region = "eu-central-1"
-  }
-}
--- web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt install wget unzip apache2 -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
-wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
-unzip -o 2117_infinite_loop.zip
-sudo cp -r 2117_infinite_loop/* /var/www/html/
-sudo systemctl restart apache2
