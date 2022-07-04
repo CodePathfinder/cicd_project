520742edac5a177661a7d2e7ccd71f73753bd572
Gennadiy Molchanov <gamolchanov@gmail.com>
2022-07-04 20:32:03 +0300

    public cidrs corrected

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "network_prod" {
   source               = "./aws_modules/network"
   vpc_cidr             = "10.1.0.0/16"
   env                  = "prod"
-  public_ids           = ["10.1.10.0/24", "10.1.11.0/24"]
+  public_subnet_cidrs  = ["10.1.10.0/24", "10.1.11.0/24"]
   private_subnet_cidrs = ["10.1.20.0/24", "10.1.21.0/24"]
 }
 
